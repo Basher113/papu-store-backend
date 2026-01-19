bda50c58b1f0fa30a80e9de2e0e734b464abf0dd
Basher113 <147272612+Basher113@users.noreply.github.com>
2026-01-19 13:33:40 +0800

    Fix prisma constructor error

--- src/db.js
+++ src/db.js
@@ -1,8 +1,6 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient({
-  adapter: {
-    url: process.env.DATABASE_URL, 
-  },
+  datasourceUrl: process.env.DATABASE_URL
 });
 
 module.exports = prisma;
